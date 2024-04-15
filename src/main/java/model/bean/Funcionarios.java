91af49e844de3a047b529199426b4cb2f950ce1f
Renan Santos <renancardoso1414@gmail.com>
2024-04-15 20:20:15 -0300

    Create Funcionarios.java

---
+++ src/main/java/model/bean/Funcionarios.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.bean;
+
+/**
+ *
+ * @author renan
+ */
+public class Funcionarios {
+    private int id_funcionarios;
+    private String nomeFuncionario;
+    private String email;
+    private String cargo;
+    private String cpf;
+
+    public int getId_funcionarios() {
+        return id_funcionarios;
+    }
+
+    public void setId_funcionarios(int id_funcionarios) {
+        this.id_funcionarios = id_funcionarios;
+    }
+
+    public String getNomeFuncionario() {
+        return nomeFuncionario;
+    }
+
+    public void setNomeFuncionario(String nomeFuncionario) {
+        this.nomeFuncionario = nomeFuncionario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+    
+}
