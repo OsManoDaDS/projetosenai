9e6caa3be42613d30235a0d37c247a36adc59c2c
Renan Santos <renancardoso1414@gmail.com>
2024-04-15 22:18:39 -0300

    fix ConnectionFactory

---
+++ src/main/java/connection/ConnectionFactory.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author renan
+ */
+public class ConnectionFactory {
+    private final String DRIVER = "com.mysql.cj.jdbc.Driver"; 
+    private final String URL = "jdbc:mysql://localhost:3306/mercadinhoBomDelicia";
+    private final String USER = "root";
+    private final String PASS = "1234";
+    
+    public Connection getConnection(){
+        try{  
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        }catch (ClassNotFoundException | SQLException ex){
+            throw new RuntimeException("Erro na Conexão: ", ex);
+            
+        }
+        
+    }
+    
+    public static void closeConnection (Connection con){
+        try{    
+            if(con!=null){           
+                con.close();
+            }
+        }catch (SQLException ex){
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        }    
+    
+    public static void closeConnection (Connection con, PreparedStatement stmt){
+        
+        closeConnection(con);
+        
+        try{    
+            
+           if(stmt != null){ 
+               stmt.close(); 
+            }
+           
+        }catch (SQLException ex){
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        }    
+    
+      
+    public static void closeConnection (Connection con, PreparedStatement stmt, ResultSet rs){
+        
+        closeConnection(con, stmt);
+        
+        try{    
+            if(rs != null){ 
+               rs.close();   
+            }
+           
+        }catch (SQLException ex){
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        }      
+    
+    
+    
+ 
+    
+    }
+  
+    
+
+
+
+
+
+        
