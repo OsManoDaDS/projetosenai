626218b09409c81c048aec0b7e2e8b901d059737
Pedro Portales <64710575+pedroportales@users.noreply.github.com>
2024-04-16 09:24:21 -0300

    Update ConnectionFactory.java
    static method

--- src/main/java/connection/ConnectionFactory.java
+++ src/main/java/connection/ConnectionFactory.java
@@ -17,12 +17,12 @@ import java.util.logging.Logger;
  * @author renan
  */
 public class ConnectionFactory {
-    private final String DRIVER = "com.mysql.cj.jdbc.Driver"; 
-    private final String URL = "jdbc:mysql://localhost:3306/mercadinhoBomDelicia";
-    private final String USER = "root";
-    private final String PASS = "1234";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver"; 
+    private static final String URL = "jdbc:mysql://localhost:3306/mercadinhoBomDelicia";
+    private static final String USER = "root";
+    private static final String PASS = "1234";
     
-    public Connection getConnection(){
+    public static Connection getConnection(){
         try{  
             Class.forName(DRIVER);
             return DriverManager.getConnection(URL, USER, PASS);
